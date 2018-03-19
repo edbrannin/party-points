4b2233e8e173f1e3e26d95461cc27caf09a5d937
Ed Brannin <edbrannin@gmail.com>
2018-03-18 22:52:52 -0400

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">MLM Party Points</h1>
+          <h1 className="App-title">Party Points Raffle</h1>
         </header>
         <People />
       </div>
